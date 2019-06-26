976a569e2590b0090a97244c20699992fc22bf73
Nikolay Perov <nikolayp2013@gmail.com>
2019-06-26 09:01:17 +0400

    initial project version

---
+++ App/src/sample/SignUpController.java
@@ -0,0 +1,48 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private TextField signUpLastname;
+
+    @FXML
+    private TextField signUpName;
+
+    @FXML
+    private TextField signUpCountry;
+
+    @FXML
+    private CheckBox signUpCheckBpxMale;
+
+    @FXML
+    private CheckBox signUpCheckBpxFemale;
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # java_profi 
 Уроки Java для профессионалов  <br />
-Текущий изученный урок: 7 <br />
-https://itproger.com/course/java-intermediate/7 <br />
+Текущий изученный урок: 8 <br />
+https://itproger.com/course/java-intermediate/8 <br />
 
 Урок #1 - Программирование на Java <br />
 Урок #2 - Массивы данных ArrayList и LinkedList <br />
